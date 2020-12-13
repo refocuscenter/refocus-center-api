88a73df528a87161b27ad7849c1c6db35afa15e5
Conrado Pinheiro <conradopsa@gmail.com>
2020-12-13 07:12:33 -0300

    purchase endpoint

--- src/controllers/purchaseController.ts
+++ src/controllers/purchaseController.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 
 import DigitalAccount from '../models/digitalAccount';
 
-import { FindOptions } from 'sequelize';
+import { FindOptions, literal } from 'sequelize';
 import { checkInstance } from '../utils/sequelizeUtils';
 import ProductsBasket from '../models/productsBasket';
 import PurchasedBasket from '../models/purchasedBasket'
@@ -14,16 +14,17 @@ const DIGITAL_ACCOUNT_NOT_FOUND = "Digital Account not found"
 
 export default class PurchaseController {
 
-    static async purchase(request: Request, response: Response) {
+    static async buyBasket(request: Request, response: Response) {
 
         /*
         
         */
+        const { account_id } = request.body;
 
         try {
-            const { idDigitalAccount, idProductsBasket } = request.params;
+            const { idProductsBasket } = request.params;
 
-            const digitalAccount = await DigitalAccount.findByPk(idDigitalAccount);
+            const digitalAccount = await DigitalAccount.findByPk(account_id);
             const productsBasket = await ProductsBasket.findByPk(idProductsBasket);
 
             if (!digitalAccount)
@@ -32,28 +33,77 @@ export default class PurchaseController {
             if (!productsBasket)
                 return responseError404(response, PRODUCT_BASKET_NOT_FOUND);
 
-            //SUM
+            const purchasedBasket = await PurchasedBasket.create({
+                idDigitalAccount: account_id,
+                idProductsBasket: idProductsBasket,
+                totalValue: await ProductsBasket.findAll({ attributes: [[literal('SUM(amount * value)'), "total"]] })
+            });
 
-            /*
-            idDigitalAccount!: string; //FK
-    idProductsBasket!: number; //FK
-    totalValue!: number;
-            */
+            const newDigitalAccount = await digitalAccount.update({ balance: digitalAccount.balance - purchasedBasket.totalValue })
 
-            /*const purchasedBasket = PurchasedBasket.create({
-                idDigitalAccount: "",
-                idProductsBasket: "",
-                totalValue: await ProductsBasket.scope('sum').
-            });*/
-
-
+            if (newDigitalAccount.balance < 0)
+                response.status(400).json({
+                    "message": "Saldo insuficiente.",
+                    "balance": {
+                        "amount": digitalAccount.balance,
+                        "currency_code": 986
+                    },
+                    "code": 530
+                });
 
             response.json({
                 "message": "Operação realizada com sucesso.",
                 "code": 0,
                 "authorization_id": 123456,
                 "balance": {
-                    "amount": 9999999,
+                    "amount": newDigitalAccount.balance,
+                    "currency_code": 986
+                }
+            });
+        } catch (error) {
+            responseError500(error, response);
+        }
+
+    }
+
+    static async purchase(request: Request, response: Response) {
+
+        try {
+            const { purchase_id, account_id, psProductCode, card, total_amount } = request.body;
+            //const { paysmart_id, issuer_id, pan, panseq } = card;
+
+            const { amount, currency_code } = total_amount;
+
+            const digitalAccount = await DigitalAccount.findByPk(account_id);
+
+            if (!digitalAccount)
+                return response.status(404).json({
+                    "message": "Número único de conta não encontrado.",
+                    "code": 111
+                  })
+
+            //[Validar cartão]
+
+            digitalAccount.balance -= amount;
+
+            if (digitalAccount.balance < 0)
+                return response.status(400).json({
+                    "message": "Saldo insuficiente.",
+                    "balance": {
+                        "amount": digitalAccount.balance,
+                        "currency_code": 986
+                    },
+                    "code": 530
+                });
+
+            const newDigitalAccount = await digitalAccount.save();
+
+            response.json({
+                "message": "Operação realizada com sucesso.",
+                "code": 0,
+                "authorization_id": 123456,
+                "balance": {
+                    "amount": newDigitalAccount.balance,
                     "currency_code": 986
                 }
             });
--- src/routers/purchase.ts
+++ src/routers/purchase.ts
@@ -3,6 +3,7 @@ import PurchaseController from '../controllers/purchaseController';
 
 const router = Router();
 
+router.post('/buyBasket/:idProductsBasket', PurchaseController.buyBasket);
 router.post('/purchase', PurchaseController.purchase);
 
 export default router
