25c3e74b4341164a6ff36edc97a8f4b90793b4a4
Conrado Pinheiro <conradopsa@gmail.com>
2020-12-12 22:22:36 -0300

    Product relationship

--- src/models/suppliedProduct.ts
+++ src/models/suppliedProduct.ts
@@ -3,11 +3,14 @@ import { Sequelize, ModelAttributes, InitOptions, DataTypes, FindOptions } from 
 import { SuperModel } from '.';
 import Product from './product';
 import ProductBasket from './productBasket';
+import Store from './store';
 
 export default class SuppliedProduct extends SuperModel {
     id!: number;
     value!: number;
     idProductBasket!: number; //FK
+    idProduct!: number; //FK
+    idStore!: number; //FK
 
     static basicAttributes = ['id', 'value'];
 
@@ -24,12 +27,16 @@ export default class SuppliedProduct extends SuperModel {
 
     static associate() {
         SuppliedProduct.hasMany(ProductBasket, { foreignKey: 'idProductBasket' })
+        SuppliedProduct.belongsTo(Product, { foreignKey: 'idProduct' })
+        SuppliedProduct.belongsTo(Store, { foreignKey: 'idStore' })
     }
 
     private static attributes: ModelAttributes = {
         id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true },
         value: { type: DataTypes.FLOAT, allowNull: false },
-        productBasketId: { type: DataTypes.BIGINT, references: { model: ProductBasket, key: 'id' } }
+        productBasketId: { type: DataTypes.BIGINT, references: { model: ProductBasket, key: 'id' } },
+        idProduct: { type: DataTypes.BIGINT, references: { model: Product, key: 'id' } },
+        idStore: { type: DataTypes.BIGINT, references: { model: Store, key: 'id' } }
     };
 
     private static defaultScope: FindOptions<any> = {
