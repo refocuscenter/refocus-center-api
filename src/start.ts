36625f1c3ee72f75cd9ab2b4bf90377d3e1c1822
Conrado Pinheiro <conradopsa@gmail.com>
2021-05-01 22:55:16 -0300

    rename poor name

--- src/start.ts
+++ src/start.ts
@@ -5,10 +5,10 @@ import { toBoolean } from "./utils/convert";
 
 dotenv.config(); //Init .env
 const SERVER_PORT = (process.env.PORT || 5445) as number;
-const MOCK_DATABASE = toBoolean(process.env.MOCK_DATABASE);
+const RC_MOCK_DATABASE = toBoolean(process.env.RC_MOCK_DATABASE);
 
 console.clear();
 
-const server =  new Server(SERVER_PORT, MOCK_DATABASE);
+const server =  new Server(SERVER_PORT, RC_MOCK_DATABASE);
     
 server.start();
